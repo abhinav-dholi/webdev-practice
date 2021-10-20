a6bc6cb6643775452c04cb37ba8c7eeab2a3b8fc
abhinav-dholi <abhinav.dholi2019@vitstudent.ac.in>
2021-10-20 17:57:18 +0530

    final

--- dancewebsite/app.js
+++ dancewebsite/app.js
@@ -2,6 +2,27 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 8000;
+const bodyParser = require("body-parser")
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/contactDance');
+}
+
+//define mongoose schema 
+
+var contactSchema = new mongoose.Schema({ 
+    name: String,
+    phone: Number,
+    email: String,
+    address: String,
+    description: String
+});
+
+var Contact = mongoose.model('Contact', contactSchema);
+
 
 
 // EXPRESS SPECIFIC STUFF
@@ -23,6 +44,18 @@ app.get('/contact', (req, res)=>{
     const params = {}
     // res.status(200).render('index.pug', params);
     res.status(200).render('contact.pug', params);
+})
+
+
+app.post('/contact', (req, res)=>{
+    var myData = new Contact(req.body);
+    myData.save().then(()=>{
+        res.send('This item has been saved to the database')
+    }).catch(()=>{
+        res.status(404).send('Item couldnt be sent to the database')
+    })
+    // res.status(200).render('index.pug', params);
+    // res.status(200).render('contact.pug');
 })
 
 // START THE SERVER
--- dancewebsite/dist/app.dev.js
+++ dancewebsite/dist/app.dev.js
@@ -5,7 +5,41 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var port = 8000; // EXPRESS SPECIFIC STUFF
+var port = 8000;
+
+var bodyParser = require("body-parser");
+
+var mongoose = require('mongoose');
+
+main()["catch"](function (err) {
+  return console.log(err);
+});
+
+function main() {
+  return regeneratorRuntime.async(function main$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.next = 2;
+          return regeneratorRuntime.awrap(mongoose.connect('mongodb://localhost:27017/contactDance'));
+
+        case 2:
+        case "end":
+          return _context.stop();
+      }
+    }
+  });
+} //define mongoose schema 
+
+
+var contactSchema = new mongoose.Schema({
+  name: String,
+  phone: Number,
+  email: String,
+  address: String,
+  description: String
+});
+var Contact = mongoose.model('Contact', contactSchema); // EXPRESS SPECIFIC STUFF
 
 app.use('/static', express["static"]('static')); // For serving static files
 
@@ -25,6 +59,15 @@ app.get('/contact', function (req, res) {
   var params = {}; // res.status(200).render('index.pug', params);
 
   res.status(200).render('contact.pug', params);
+});
+app.post('/contact', function (req, res) {
+  var myData = new Contact(req.body);
+  myData.save().then(function () {
+    res.send('This item has been saved to the database');
+  })["catch"](function () {
+    res.status(404).send('Item couldnt be sent to the database');
+  }); // res.status(200).render('index.pug', params);
+  // res.status(200).render('contact.pug');
 }); // START THE SERVER
 
 app.listen(port, function () {
